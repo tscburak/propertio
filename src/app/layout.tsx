3a9f66ffe5cd59cd07e1bfee84e87cfe11d760a7
Burak <buraktasci2001@gmail.com>
2025-07-09 19:32:58 +0300

    feat: init Shopify Polaris

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "@/styles/globals.css";
+import { AppProvider } from "@shopify/polaris";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,6 +24,24 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <AppProvider
+      i18n={{
+        Polaris: {
+          ResourceList: {
+            sortingLabel: 'Sort by',
+            defaultItemSingular: 'item',
+            defaultItemPlural: 'items',
+            showing: 'Showing {itemsCount} {resource}',
+            Item: {
+              viewItem: 'View details for {itemName}',
+            },
+          },
+          Common: {
+            checkbox: 'checkbox',
+          },
+        },
+      }}
+    >
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
@@ -30,5 +49,6 @@ export default function RootLayout({
         {children}
       </body>
     </html>
+    </AppProvider>
   );
 }
